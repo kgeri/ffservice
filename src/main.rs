cbef5a78f79c123b45e825110e21082c93450573
gergo <kiss.gergely@gmail.com>
2023-04-02 14:10:52 +0000

    cleanup, exporting to jpg

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate ffmpeg_next as ffmpeg;
+extern crate image;
 
 use ffmpeg::{codec, format, format::context::*, media::Type, software, util::frame::Video};
-use std::{fs::File, io::Write};
 
 fn main() -> Result<(), ffmpeg::Error> {
     ffmpeg::init().unwrap();
@@ -26,7 +26,6 @@ fn thumbnail(context: &mut Input) -> Result<(), ffmpeg::Error> {
 
     let context_decoder = codec::Context::from_parameters(input.parameters())?;
     let mut decoder = context_decoder.decoder().video()?;
-
     let mut scaler = software::scaling::context::Context::get(
         decoder.format(),
         decoder.width(),
@@ -37,33 +36,32 @@ fn thumbnail(context: &mut Input) -> Result<(), ffmpeg::Error> {
         software::scaling::Flags::BILINEAR,
     )?;
 
-    let mut process_decoded_frames =
-        |decoder: &mut ffmpeg::decoder::Video| -> Result<(), ffmpeg::Error> {
-            let mut decoded = Video::empty();
-            if decoder.receive_frame(&mut decoded).is_ok() {
-                let mut rgb_frame = Video::empty();
-                scaler.run(&decoded, &mut rgb_frame)?;
-                save_file(&rgb_frame, 0).unwrap();
-            }
-            Ok(())
-        };
+    let packets = context
+        .packets()
+        .filter(|(stream, _)| stream.index() == video_stream_index)
+        .map(|(_, packet)| packet);
 
-    for (stream, packet) in context.packets() {
-        if stream.index() == video_stream_index {
-            decoder.send_packet(&packet)?;
-            process_decoded_frames(&mut decoder)?;
+    for packet in packets {
+        decoder.send_packet(&packet)?;
+        let mut decoded = Video::empty();
+        if decoder.receive_frame(&mut decoded).is_ok() {
+            let mut frame = Video::empty();
+            scaler.run(&decoded, &mut frame)?;
+
+            image::save_buffer(
+                "frame.jpg",
+                frame.data(0),
+                frame.width(),
+                frame.height(),
+                image::ColorType::Rgb8,
+            )
+            .unwrap(); // TODO better error reporting
+            break;
         }
     }
     decoder.send_eof()?;
     // process_decoded_frames(&mut decoder);
 
-    Ok(())
-}
-
-fn save_file(frame: &Video, index: usize) -> std::result::Result<(), std::io::Error> {
-    let mut file = File::create(format!("frame{}.ppm", index))?;
-    file.write_all(format!("P6\n{} {}\n255\n", frame.width(), frame.height()).as_bytes())?;
-    file.write_all(frame.data(0))?;
     Ok(())
 }
 
